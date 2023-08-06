2210bd55ae8977ff7f9752703a72f8c50c9c2f1f
Billy Burch <billyburch40@gmail.com>
2023-08-06 11:58:45 -0600

    define custom methods

--- final_practice.rb
+++ final_practice.rb
@@ -10,11 +10,19 @@
 # How many arguments did you pass your method?
 
 
+
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# The return value of the method named custom_greeting is an implicit return of the string `Well hello there #(name). Hope you're having a good one!` Interpolation is used to embed the input argument into the return.
+# I passed three different string value arguments to the method greeting.
+
+def custom_greeting(name)
+    "Well hello there #{name}. Hope you're having a good one!"
+end
+
+p custom_greeting("Aimee")
+p custom_greeting("Billy")
+p custom_greeting("Frank")
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
