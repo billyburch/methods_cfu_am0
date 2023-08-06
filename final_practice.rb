2d1c409eefe56bcbb3b20ae942016f86fdeff8fb
Billy Burch <billyburch40@gmail.com>
2023-08-06 14:57:43 -0600

    add final practice code and eval answers

--- final_practice.rb
+++ final_practice.rb
@@ -41,32 +41,66 @@ puts neighbor
 # What is the return value of your method?
 # How many arguments did you pass your method?
 # What data type was your argument(s)?
-def greet_person(first)(middle)(last)
-    "Hello #{first middle last}!"
+def greet_person(first, middle, last)
+  "Hello #{first}#{middle}#{last}!"
 end
 
-p greet_person "Frank", "Lloyd", "Wright"
+architect = greet_person("Frank ", "Lloyd ", "Wright")
+rbg = greet_person("Ruth ", "Bader ", "Ginsburg")
+
+p architect
+p rbg
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# The return values for method square, given the 3 integer arguments I passed to it, were integers 4, 16, and 36
+# My arguments were all data type integer.
+
+def square(num)
+    num * num
+ end
+
+two = square(2)
+four = square(4)
+six = square(6)
+
+ p "Your return values are #{two}, #{four}, #{six}!"
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-check_stock(4, "Coffee");
-# => "Coffee is stocked"
+def check_stock(inventory, product)
 
-check_stock(3, "Tortillas");
-# => "Tortillas - running LOW"
+    if inventory >= 4 
+        then p "#{product} is stocked"
+    elsif inventory <= 3 && inventory > 0 
+        then p "#{product} - running LOW"
+    else inventory = 0 
+        p "#{product} - OUT of stock"
+    end
+end
 
-check_stock(0, "Cheese");
-# => "Cheese - OUT of stock!"
+coffee = check_stock(4, "Coffee")
+tortillas = check_stock(3, "Tortillas")
+cheese = check_stock(0, "Cheese")
+salsa = check_stock(1, "Salsa")
 
-check_stock(1, "Salsa");
-# => "Salsa - running LOW"
+p coffee
+p tortillas
+p cheese
+p salsa
+
+# check_stock(4, "Coffee");
+# # => "Coffee is stocked"
+
+# check_stock(3, "Tortillas");
+# # => "Tortillas - running LOW"
+
+# check_stock(0, "Cheese");
+# # => "Cheese - OUT of stock!"
+
+# check_stock(1, "Salsa");
+# # => "Salsa - running LOW"
--- self_eval.md
+++ self_eval.md
@@ -1,4 +1,6 @@
 # Self-Evaluation
 
 - Reflect on your learning and work today. What are you most proud of?
+*I'm incredibly proud to have pushed through the methods lesson, practices, and assignments. I needed to advocate to other professionals and student-colleagues to talk through some of my mental blocks. I'm slowly but surely beginning to feel a little more ownership and autonomy with the syntax I'm writing.*
 - What is the power of using methods in our applications?
+*Methods allow us to use pre-determined or independently created instructions in our programming. This boosts our efficiency and allows us to focus on the greater picture without having to write and re-write action specific code over and over.*
